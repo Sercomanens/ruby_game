acc9c7e4ac2dd236978b83de4a106c1aea4e81fa
Laurent Sercomanens <aurovici@gmail.com>
2024-08-06 09:16:56 +0530

    update ruby_game.rb

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,2 +1,23 @@
 puts "Let's play a game!
 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀"
+
+
+player1 = "Finn"
+health1 = 60
+
+puts message = "#{player1} has a health of #{health1}."
+
+puts message.center(50, '*')
+puts player1.ljust(10, '.') + health1.to_s.rjust(10, '.')
+puts message = "#{player1.upcase} has a health of #{health1}."
+
+
+def say_hello(name, health=100)
+  "I'm #{name.capitalize} with a health of #{health}."
+end
+
+
+puts say_hello("finn", 60)
+puts say_hello("lucy", 90)
+puts say_hello("jase")
+puts say_hello("alex", 125)
